047411c955b77a8c33029ac8375fe83545567b83
PaulKutka <kutka100@gmail.com>
2017-03-19 19:01:27 +0200

    Added template path and removed logging

--- src/main/java/lt/damss/controller/MainController.java
+++ src/main/java/lt/damss/controller/MainController.java
@@ -3,8 +3,6 @@ package lt.damss.controller;
 import lt.damss.models.RegistrationForm;
 import lt.damss.repositories.RegistrationFormRepository;
 import lt.damss.service.NotificationService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.mail.MessagingException;
-import java.util.Locale;
+
 
 /**
  * Created by paulius on 17.3.11.
@@ -20,7 +18,7 @@ import java.util.Locale;
 @RestController
 public class MainController {
 
-    private Logger logger = LoggerFactory.getLogger(MainController.class);
+    //private Logger logger = LoggerFactory.getLogger(MainController.class);
 
     @Autowired
     private RegistrationFormRepository repository;
@@ -43,7 +41,7 @@ public class MainController {
             notificationService.sendNotification(form);
         } catch (MessagingException e){
             // cath error
-            logger.info("Error Sending Email:" + e.getMessage());
+            //logger.info("Error Sending Email:" + e.getMessage());
         }
 
        return form;
