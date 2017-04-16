61a5b1c2340998f283f2584fe9a54807d6c377f6
PaulKutka <kutka100@gmail.com>
2017-04-16 12:25:56 +0300

    Cleared unecessary lines

--- src/main/java/lt/damss/config/MyApplicationConfiguration.java
+++ src/main/java/lt/damss/config/MyApplicationConfiguration.java
@@ -9,26 +9,5 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MyApplicationConfiguration {
 
-//    @Bean
-//    @Autowired
-//    public EntityManagerFactory entityManagerFactory(DataSource dataSource) {
-//        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-//        vendorAdapter.setGenerateDdl(true);
-//
-//        Properties jpaProperties = new Properties();
-//        jpaProperties.setProperty("hibernate.show_sql", showsql);
-//        jpaProperties.setProperty("hibernate.dialect", dialect);
-//
-//        LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-//        localContainerEntityManagerFactoryBean.setJpaVendorAdapter(vendorAdapter);
-//        localContainerEntityManagerFactoryBean.setPackagesToScan("your packages....");
-//        localContainerEntityManagerFactoryBean.setDataSource(dataSource);
-//        localContainerEntityManagerFactoryBean.setJpaProperties(jpaProperties);
-//        localContainerEntityManagerFactoryBean.afterPropertiesSet();
-//
-//        return localContainerEntityManagerFactoryBean.getObject();
-//    }
-
-
 
 }
